943d172b43da7ff14774f5aac57aad317b19f811
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-07-02 17:14:11 -0500

    Proyecto Web Demo

---
+++ demoMVC/demoMVC/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demoMVC.Models;
+
+namespace demoMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student student = new Student();
+            student.Nombre = "Julio";
+
+
+            return View(student);
+            //return Content("Hola mundo");
+
+
+        }
+    }
+}
