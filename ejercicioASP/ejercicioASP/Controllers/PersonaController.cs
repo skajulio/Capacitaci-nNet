87e18c36d9e00bc01248e0cd7ecb3d036bd76ba6
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-07-03 09:05:43 -0500

    Actualizacion de archivos y creación de proyecto ASP de tarea

---
+++ ejercicioASP/ejercicioASP/Controllers/PersonaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ejercicioASP.Models;
+
+namespace ejercicioASP.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Index()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Julio";
+            persona.Id = 123;
+            persona.Edad = 27;
+            persona.Genero = 'H';
+            
+            return View(persona);
+        }
+    }
+}
---
+++ ejercicioASP/ejercicioASP/Models/Persona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ejercicioASP.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Id { get; set; }
+        public char Genero { get; set; }
+    }
+}
